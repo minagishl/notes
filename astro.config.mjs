54c87df285d08dc3c44138a3c369629f539db77a
minagishl <mvxd568.co@icloud.com>
2024-11-19 01:52:31 +0900

    Create Header

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,10 @@ import sitemap from "@astrojs/sitemap";
 
 import tailwind from "@astrojs/tailwind";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), icon()],
 });
