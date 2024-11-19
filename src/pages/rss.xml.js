95de6ddb4d1d535d27c4784f6874840d79caac5b
minagishl <mvxd568.co@icloud.com>
2024-11-19 22:11:03 +0900

    Fix rss process

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,14 +3,26 @@ import { getCollection } from "astro:content";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
 export async function GET(context) {
+  let items = [];
+
   const posts = await getCollection("articles");
+  items = posts.map((post) => ({
+    ...post.data,
+    link: `/articles/${post.slug}/`,
+  }));
+
+  const pages = await getCollection("other");
+  items = items.concat(
+    pages.map((post) => ({
+      ...post.data,
+      link: `/${post.slug}/`,
+    })),
+  );
+
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
-    items: posts.map((post) => ({
-      ...post.data,
-      link: `/articles/${post.slug}/`,
-    })),
+    items: items,
   });
 }
