0bc78130b804f7c37e7ae4dcbdf4dbda8ea8f34d
minagishl <mvxd568.co@icloud.com>
2025-02-10 10:20:12 +0900

    Added table of contents function

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,8 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
 import sitemap from "@astrojs/sitemap";
-
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
