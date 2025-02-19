f2d7fe66694323034f9fd9529fed02a062b8090c
minagishl <mvxd568.co@icloud.com>
2025-02-19 23:50:45 +0900

    feat: changed the way screen size is specified

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      minHeight: {
+        screen: "100dvh",
+      },
+      height: {
+        screen: "100dvh",
+      },
+      minWidth: {
+        screen: "100dvw",
+      },
+      width: {
+        screen: "100dvw",
+      },
+    },
+  },
+  plugins: [],
+};
