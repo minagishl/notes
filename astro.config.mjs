6d10bda8ce2436a01bac42b0b3e5ee940d6a819e
minagishl <mvxd568.co@icloud.com>
2025-03-04 21:40:58 +0900

    build: add compression tool

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import react from "@astrojs/react";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
@@ -26,6 +27,7 @@ export default defineConfig({
     tailwind(),
     icon(),
     react(),
+    compress(),
   ],
   markdown: {
     shikiConfig: {
